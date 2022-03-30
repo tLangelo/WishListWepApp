6dbc770f9ec0cb5a9b912a73e2d76a3d9c7a21a6
Christian Sandholdt Trip <90309082+ChristianTrip@users.noreply.github.com>
2022-03-30 14:33:31 +0200

    commit

--- src/main/java/com/example/wishlistwepapp/controllers/IndexController.java
+++ src/main/java/com/example/wishlistwepapp/controllers/IndexController.java
@@ -1,14 +1,28 @@
 package com.example.wishlistwepapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/index")
-    public String index(){
+    @GetMapping("/")
+    public String index(Model myModel){
+        myModel.addAttribute("sign_in", "jdfkjdk");
         return "index";
+    }
+
+    @PostMapping("sign_in")
+    public String redirectSignIN(){
+        return "redirect:/logIn";
+    }
+
+    @GetMapping("/logIn")
+    public String signIn(){
+        return "signIn";
     }
 
 }
